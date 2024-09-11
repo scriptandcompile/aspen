8c15f204593698a12fa3990903af0b60c718744f
ScriptAndCompile <scriptandcompile@gmail.com>
2024-09-11 14:11:35 -0700

    Added support for ignoring parsing forms, references, classes, and modules.

--- src/check.rs
+++ src/check.rs
@@ -7,16 +7,28 @@ use walkdir::WalkDir;
 
 use vb6parse::parsers::{VB6ClassFile, VB6FormFile, VB6ModuleFile, VB6Project};
 
-pub fn check_subcommand(project_path_argument: PathBuf) -> Result<()> {
-    if !project_path_argument.exists() {
-        println!("No project file found at '{:?}'", project_path_argument);
+pub struct CheckSettings {
+    pub project_path: PathBuf,
+    pub check_forms: bool,
+    pub check_modules: bool,
+    pub check_classes: bool,
+    pub check_references: bool,
+}
+
+pub fn check_subcommand(check_settings: CheckSettings) -> Result<()> {
+    if !check_settings.project_path.exists() {
+        println!(
+            "No project file found at '{:?}'",
+            check_settings.project_path
+        );
         return Ok(());
     }
 
     let mut error_count = 0;
     let mut project_count = 1;
-    if project_path_argument.is_dir() {
-        let search_path = project_path_argument.to_str().unwrap();
+
+    if check_settings.project_path.is_dir() {
+        let search_path = check_settings.project_path.to_str().unwrap();
         let walker = WalkDir::new(search_path).into_iter();
 
         println!("Searching '{}' for .vbp project files.", search_path);
@@ -35,13 +47,18 @@ pub fn check_subcommand(project_path_argument: PathBuf) -> Result<()> {
                 continue;
             }
 
-            let project_path = project_path.as_ref().unwrap().path();
+            let check_settings = CheckSettings {
+                project_path: project_path.as_ref().unwrap().path().to_path_buf(),
+                check_forms: check_settings.check_forms,
+                check_modules: check_settings.check_modules,
+                check_classes: check_settings.check_classes,
+                check_references: check_settings.check_references,
+            };
 
-            error_count += check_project(project_path)?;
+            error_count += check_project(&check_settings)?;
         }
     } else {
-        let project_path = project_path_argument.as_path();
-        error_count += check_project(project_path)?;
+        error_count += check_project(&check_settings)?;
     }
 
     if project_count == 1 {
@@ -87,22 +104,23 @@ fn join_parent_project_path(parent_project_path: &Path, file_path: &str) -> Path
 // This will allow us to display the errors in a more structured way.
 // For now we just print the errors to the console and return the error count.
 
-fn check_project(project_path: &Path) -> Result<u32> {
-    let project_contents = std::fs::read(project_path).unwrap();
+fn check_project(check_settings: &CheckSettings) -> Result<u32> {
+    let project_contents = std::fs::read(&check_settings.project_path).unwrap();
     let mut error_count = 0;
 
-    let project_file_name = std::path::Path::new(project_path)
+    let file_name = check_settings
+        .project_path
         .file_name()
         .unwrap()
         .to_str()
         .unwrap();
 
-    let project = VB6Project::parse(project_file_name, project_contents.as_slice());
+    let project = VB6Project::parse(file_name, project_contents.as_slice());
 
     if project.is_err() {
         println!(
             "Failed to load project '{}'\r\n{}",
-            project_path.to_str().unwrap(),
+            check_settings.project_path.to_str().unwrap(),
             project.err().unwrap()
         );
         return Ok(error_count);
@@ -111,118 +129,150 @@ fn check_project(project_path: &Path) -> Result<u32> {
     let project = project.unwrap();
 
     //remove filename from path
-    let project_directory = std::path::Path::new(project_path).parent().unwrap();
+    let project_directory = std::path::Path::new(&check_settings.project_path)
+        .parent()
+        .unwrap();
 
-    for reference in project.get_project_references() {
-        match reference {
-            VB6ProjectReference::Project { path } => {
-                let reference_path = join_parent_project_path(project_directory, &path.to_string());
-                if std::fs::metadata(&reference_path).is_err() {
-                    println!(
-                        "{} | Sub-Project Reference not found: {}",
-                        project_path.to_str().unwrap(),
-                        reference_path.to_str().unwrap()
-                    );
-                    error_count += 1;
+    if check_settings.check_references {
+        for reference in project.get_project_references() {
+            match reference {
+                VB6ProjectReference::Project { path } => {
+                    let reference_path =
+                        join_parent_project_path(project_directory, &path.to_string());
+                    if std::fs::metadata(&reference_path).is_err() {
+                        println!(
+                            "{} | Sub-Project Reference not found: {}",
+                            check_settings.project_path.to_str().unwrap(),
+                            reference_path.to_str().unwrap()
+                        );
+                        error_count += 1;
+                    }
                 }
+                _ => unreachable!(),
             }
-            _ => unreachable!(),
         }
+    } else {
+        println!(
+            "Skipping reference checks for '{}'",
+            check_settings.project_path.to_str().unwrap()
+        );
     }
 
-    for class_reference in project.classes {
-        let class_path =
-            join_parent_project_path(project_directory, &class_reference.path.to_string());
+    if check_settings.check_classes {
+        for class_reference in project.classes {
+            let class_path =
+                join_parent_project_path(project_directory, &class_reference.path.to_string());
 
-        if std::fs::metadata(&class_path).is_err() {
-            println!(
-                "{} | Class not found: {}",
-                project_path.to_str().unwrap(),
-                class_path.to_str().unwrap()
-            );
-            error_count += 1;
-            continue;
+            if std::fs::metadata(&class_path).is_err() {
+                println!(
+                    "{} | Class not found: {}",
+                    check_settings.project_path.to_str().unwrap(),
+                    class_path.to_str().unwrap()
+                );
+                error_count += 1;
+                continue;
+            }
+
+            let file_name = class_path.file_name().unwrap().to_str().unwrap();
+            let class_contents = std::fs::read(&class_path).unwrap();
+            let class = VB6ClassFile::parse(file_name.to_owned(), &mut class_contents.as_slice());
+
+            if class.is_err() {
+                println!(
+                    "{} | Failed to load class '{}' | load error: {}",
+                    check_settings.project_path.to_str().unwrap(),
+                    file_name,
+                    class.err().unwrap()
+                );
+                error_count += 1;
+                continue;
+            }
+
+            let _class = class.unwrap();
         }
-
-        let file_name = class_path.file_name().unwrap().to_str().unwrap();
-        let class_contents = std::fs::read(&class_path).unwrap();
-        let class = VB6ClassFile::parse(file_name.to_owned(), &mut class_contents.as_slice());
-
-        if class.is_err() {
-            println!(
-                "{} | Failed to load class '{}' | load error: {}",
-                project_path.to_str().unwrap(),
-                file_name,
-                class.err().unwrap()
-            );
-            error_count += 1;
-            continue;
-        }
-
-        let _class = class.unwrap();
+    } else {
+        println!(
+            "Skipping class checks for '{}'",
+            check_settings.project_path.to_str().unwrap()
+        );
     }
 
-    for module_reference in project.modules {
-        let module_path =
-            join_parent_project_path(project_directory, &module_reference.path.to_string());
+    if check_settings.check_modules {
+        for module_reference in project.modules {
+            let module_path =
+                join_parent_project_path(project_directory, &module_reference.path.to_string());
 
-        if std::fs::metadata(&module_path).is_err() {
-            println!(
-                "{} | Module not found: {}",
-                project_path.to_str().unwrap(),
-                module_path.to_str().unwrap()
-            );
-            error_count += 1;
-            continue;
+            if std::fs::metadata(&module_path).is_err() {
+                println!(
+                    "{} | Module not found: {}",
+                    check_settings.project_path.to_str().unwrap(),
+                    module_path.to_str().unwrap()
+                );
+                error_count += 1;
+                continue;
+            }
+
+            let file_name = module_path.file_name().unwrap().to_str().unwrap();
+            let module_contents = std::fs::read(&module_path).unwrap();
+            let module = VB6ModuleFile::parse(file_name.to_owned(), &module_contents);
+
+            if module.is_err() {
+                println!(
+                    "{} | Failed to load module '{}' load error: {}",
+                    check_settings.project_path.to_str().unwrap(),
+                    file_name,
+                    module.err().unwrap()
+                );
+                error_count += 1;
+                continue;
+            }
+
+            let _module = module.unwrap();
         }
-
-        let file_name = module_path.file_name().unwrap().to_str().unwrap();
-        let module_contents = std::fs::read(&module_path).unwrap();
-        let module = VB6ModuleFile::parse(file_name.to_owned(), &module_contents);
-
-        if module.is_err() {
-            println!(
-                "{} | Failed to load module '{}' load error: {}",
-                project_path.to_str().unwrap(),
-                file_name,
-                module.err().unwrap()
-            );
-            error_count += 1;
-            continue;
-        }
-
-        let _module = module.unwrap();
+    } else {
+        println!(
+            "Skipping module checks for '{}'",
+            check_settings.project_path.to_str().unwrap()
+        );
     }
 
-    for form_reference in project.forms {
-        let form_path = join_parent_project_path(project_directory, &form_reference.to_string());
+    if check_settings.check_forms {
+        for form_reference in project.forms {
+            let form_path =
+                join_parent_project_path(project_directory, &form_reference.to_string());
 
-        if std::fs::metadata(&form_path).is_err() {
-            println!(
-                "{} | Form not found: {}",
-                project_path.to_str().unwrap(),
-                form_path.to_str().unwrap()
-            );
-            error_count += 1;
-            continue;
+            if std::fs::metadata(&form_path).is_err() {
+                println!(
+                    "{} | Form not found: {}",
+                    check_settings.project_path.to_str().unwrap(),
+                    form_path.to_str().unwrap()
+                );
+                error_count += 1;
+                continue;
+            }
+
+            let file_name = form_path.file_name().unwrap().to_str().unwrap();
+            let form_contents = std::fs::read(&form_path).unwrap();
+            let form = VB6FormFile::parse(file_name.to_owned(), &mut form_contents.as_slice());
+
+            if form.is_err() {
+                println!(
+                    "{} | Failed to load form '{}' load error: {}",
+                    check_settings.project_path.to_str().unwrap(),
+                    file_name,
+                    form.err().unwrap()
+                );
+                error_count += 1;
+                continue;
+            }
+
+            let _form = form.unwrap();
         }
-
-        let file_name = form_path.file_name().unwrap().to_str().unwrap();
-        let form_contents = std::fs::read(&form_path).unwrap();
-        let form = VB6FormFile::parse(file_name.to_owned(), &mut form_contents.as_slice());
-
-        if form.is_err() {
-            println!(
-                "{} | Failed to load form '{}' load error: {}",
-                project_path.to_str().unwrap(),
-                file_name,
-                form.err().unwrap()
-            );
-            error_count += 1;
-            continue;
-        }
-
-        let _form = form.unwrap();
+    } else {
+        println!(
+            "Skipping form checks for '{}'",
+            check_settings.project_path.to_str().unwrap()
+        );
     }
 
     Ok(error_count)
--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,53 @@ use clap::{command, value_parser, Arg, Command};
 fn main() -> Result<()> {
     let matches = command!()
         .subcommand(
-            Command::new("check").about("Check the project").arg(
-                Arg::new("project")
-                    .required(false)
-                    .value_parser(value_parser!(PathBuf)),
-            ),
+            Command::new("check")
+                .about("Check the project")
+                .arg(
+                    Arg::new("ignore forms")
+                        .short('f')
+                        .long("form")
+                        .alias("forms")
+                        .required(false)
+                        .value_parser(value_parser!(bool))
+                        .action(clap::ArgAction::SetFalse)
+                        .help("skip checking the forms listed in the project"),
+                )
+                .arg(
+                    Arg::new("ignore modules")
+                        .short('m')
+                        .long("module")
+                        .alias("modules")
+                        .required(false)
+                        .value_parser(value_parser!(bool))
+                        .action(clap::ArgAction::SetFalse)
+                        .help("skip checking the modules listed in the project"),
+                )
+                .arg(
+                    Arg::new("ignore classes")
+                        .short('c')
+                        .long("class")
+                        .alias("classes")
+                        .required(false)
+                        .value_parser(value_parser!(bool))
+                        .action(clap::ArgAction::SetFalse)
+                        .help("skip checking the classes listed in the project"),
+                )
+                .arg(
+                    Arg::new("ignore references")
+                        .short('r')
+                        .long("reference")
+                        .alias("references")
+                        .required(false)
+                        .value_parser(value_parser!(bool))
+                        .action(clap::ArgAction::SetFalse)
+                        .help("skip checking the references listed in the project"),
+                )
+                .arg(
+                    Arg::new("project path")
+                        .required(false)
+                        .value_parser(value_parser!(PathBuf)),
+                ),
         )
         .arg_required_else_help(true)
         .get_matches();
@@ -23,11 +65,27 @@ fn main() -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("check") {
         let current_dir = current_dir()?;
 
-        let project_argument = matches
-            .get_one::<PathBuf>("project")
-            .unwrap_or(&current_dir);
+        let project_path = matches
+            .get_one::<PathBuf>("project path")
+            .unwrap_or(&current_dir)
+            .to_path_buf();
 
-        check_subcommand(project_argument.to_path_buf())?;
+        let check_forms = *matches.get_one::<bool>("ignore forms").unwrap_or(&false);
+        let check_modules = *matches.get_one::<bool>("ignore modules").unwrap_or(&false);
+        let check_classes = *matches.get_one::<bool>("ignore classes").unwrap_or(&false);
+        let check_references = *matches
+            .get_one::<bool>("ignore references")
+            .unwrap_or(&false);
+
+        let check_settings = check::CheckSettings {
+            project_path,
+            check_forms,
+            check_modules,
+            check_classes,
+            check_references,
+        };
+
+        check_subcommand(check_settings)?;
 
         return Ok(());
     }
